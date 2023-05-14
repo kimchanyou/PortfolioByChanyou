5cd76141d02efb7381c2722307777e1ba1cbccfc
cykim <114848800+kimchanyou@users.noreply.github.com>
2023-05-14 19:16:12 +0900

    보석 스폰 확률 조정
    보석 단계별로 스폰되는 확률 조정함
    (1단계  60%, 2단계 25%, 3단계 10%, 4단계 3.5%, 5단계 1.5%)

--- Assets/02.Scripts/GemItem.cs
+++ Assets/02.Scripts/GemItem.cs
@@ -10,7 +10,9 @@ public class GemItem : GemStat
 
     public Dictionary<int, GemInfo> dicGem = Managers.Data.dicGemData;
     
-    int id = 101;
+    public int gemId = 0;
+
+    public int choice = 0;
 
     private void Awake()
     {
@@ -19,8 +21,13 @@ public class GemItem : GemStat
 
     private void OnEnable() // SetActive(true)가 되었을 때 실행
     {
-        id = id + Random.Range(0, dicGem.Count);
-        GemInfo gemInfo = dicGem[id];
+        float[] probability = new float[5] { 60f, 25f, 10f, 3.5f, 1.5f };
+
+        choice = Choose(probability);
+
+        //id = id + Random.Range(0, dicGem.Count); // 스폰되는 보석 확률 조정은 여기서 하면 될 듯
+        gemId = choice;
+        GemInfo gemInfo = dicGem[gemId];
         Id = gemInfo.id;
         Attack = gemInfo.attack;
         Name = gemInfo.name;
@@ -33,6 +40,31 @@ public class GemItem : GemStat
     {
         GetComponentInChildren<Slider>().value = 1.0f;
         Init();
-        id = 101;
+        gemId = 0;
+    }
+
+    public int Choose(float[] probs) // float 반환, float 배열 인자로 받음 float 배열이 확률을 나타낸다.
+    {
+        float total = 0;
+
+        foreach (float elem in probs)
+        {
+            total += elem;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (int i = 0; i < probs.Length; i++)
+        {
+            if (randomPoint < probs[i])
+            {
+                return i;
+            }
+            else
+            {
+                randomPoint -= probs[i];
+            }
+        }
+        return probs.Length - 1;
     }
 }
